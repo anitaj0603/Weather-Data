5274bb0b8a6b9e9733057f9319ea6a3e976d7f98
anitaj0603 <anitaj10@nycstudents.net>
2025-01-08 14:31:27 +0000

    Error in code, nothing prints

--- WeatherData.java
+++ WeatherData.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 public class WeatherData
 {
-    /** Guaranteed not to be null and to contain only non-null entries */
+    public WeatherData(ArrayList<Double> temps)
+    {
+        temperatures = temps;    }
     private ArrayList<Double> temperatures;
 
     /**
@@ -8,7 +11,19 @@ public class WeatherData
     * lower and all values that are greater than upper, as described in part (a)
     */
     public void cleanData(double lower, double upper)
-    { /* to be implemented in part (a) */ }
+    {
+        for (int i = 0; i < temperatures.size(); i++)
+        {
+            double temp = temperatures.get(i);
+            if (temp < lower || temp > upper)
+            temperatures.remove(i);
+            i--;
+        }
+    }
+    public String toString()
+    {
+        return temperatures.toString();
+    }
 
     /**
     * Returns the length of the longest heat wave found in temperatures, as described in
@@ -16,7 +31,16 @@ public class WeatherData
     * Precondition: There is at least one heat wave in temperatures based on threshold.
     */
     public int longestHeatWave(double threshold)
-    { /* to be implemented in part (b) */ }
-    
-    // There may be instance variables, constructors, and methods that are not shown.
+    { 
+        int heatWave = 0;
+        int max = 0;
+        for (double t : temperatures)
+        if(t > threshold)
+        {
+            heatWave++;
+        }
+        if(heatWave > max) max = heatWave;
+        else heatWave = 0;
+        return max;
+    }
 }
